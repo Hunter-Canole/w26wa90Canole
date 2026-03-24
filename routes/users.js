2b8a13ada53d4302d97e022d93e16b953cf7becd
Hunter Canole <158322496+Hunter-Canole@users.noreply.github.com>
2026-03-24 12:00:53 -0500

    added a space

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ let vTwo = 15
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   vTwo = vTwo - vOne
-  res.send('Values are: ' + vOne + 'and ' + vTwo);
+  res.send('Values are: ' + vOne + ' and ' + vTwo);
 });
 
 module.exports = router;
